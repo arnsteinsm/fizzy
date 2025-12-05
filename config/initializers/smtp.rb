bde191fb10bdfc25392394e5a83ca90a2477c395
Arnstein Snyen Marjala <arnstein@giof.se>
2025-12-05 10:33:29 +0100

    Update SMTP settings to use example.com

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,13 +3,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address:              ENV.fetch("SMTP_ADDRESS", "smtp.eu.mailgun.org"),
+    address:              ENV.fetch("SMTP_ADDRESS", "smtp.example.com"),
     port:                 ENV.fetch("SMTP_PORT", 587).to_i,
     user_name:            ENV.fetch("SMTP_USERNAME"),
     password:             ENV.fetch("SMTP_PASSWORD"),
     authentication:       :plain,
     enable_starttls_auto: true,
-    domain:               ENV.fetch("SMTP_DOMAIN", "mg.marjala.no"),
+    domain:               ENV.fetch("SMTP_DOMAIN", "example.com"),
     open_timeout:         5,
     read_timeout:         5
   }
